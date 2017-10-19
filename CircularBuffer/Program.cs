fa77fecf39893077138b08377747ef0fbefacc1f
Andreas Norberg <fiskkneipp@gmial.com>
2017-10-19 13:48:42 +0200

    final

--- CircularBuffer/Program.cs
+++ CircularBuffer/Program.cs
@@ -6,7 +6,68 @@ namespace CircularBuffer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue queue;
+            bool quit = false;
+            int temp = 0;
+
+            Console.WriteLine("How big do you want your buffer to be?");
+            try
+            {
+                temp = Int32.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please use numericals. " + ex);
+            }
+
+            queue = new Queue(temp);
+
+            do
+            {                
+                Console.WriteLine("1. Add data to buffer.\n" +
+                                "2. Remove data from buffer.\n" +
+                                "3. Quit.\n" +
+                                "Enter your choice(1-3): ");
+                int input = Int32.Parse(Console.ReadLine());
+
+                switch (input)
+                {
+                    case 1:
+                        if (queue.Full())
+                        {
+                            Console.WriteLine("Buffer is full.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Enter a number to add to the buffer.");
+                            try
+                            {
+                                int addNumber = Int32.Parse(Console.ReadLine());
+                                queue.Enqueue(addNumber);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("Please use numericals. " + ex);
+                            }
+                        }                        
+                        break;
+                    case 2:
+                        if (queue.Empty())
+                        {
+                            Console.WriteLine("No data in the buffer yet.");
+                        }
+                        else
+                        {
+                            queue.Dequeue();
+                        }
+                        break;
+                    case 3:
+                        quit = true;
+                        break;
+                    default:
+                        break;
+                }
+            }while(quit == false);                        
         }
     }
 }
---
+++ CircularBuffer/Queue.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircularBuffer
+{
+    class Queue
+    {
+        private int[] buf;
+
+        private int head;
+        private int tail;
+
+        public Queue(int n)
+        {
+            buf = new int[++n];
+            head = 0;
+            tail = 0;
+        }
+
+        public void Enqueue(int put)
+        {
+                buf[head] = put;
+                head = (head + 1) % buf.Length;
+        }
+
+        public void Dequeue()
+        {
+            if (tail == head)
+            {
+                Console.WriteLine("Buffer is empty.");
+            }
+            else
+            {
+                Console.WriteLine(buf[tail] + " was removed.");
+                buf[tail] = 0;
+                tail = (tail + 1) % buf.Length;
+            }            
+        }
+
+        public bool Full()
+        {
+            if (tail == (head + 1) % (buf.Length))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Empty()
+        {
+            if (head == 0 && tail == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
